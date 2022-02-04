658bb04af157c3d4a131e85bec3a1e0591e71e5e
Jonathan Santos <jonathan01347@gmail.com>
2022-02-03 22:24:33 -0300

    restauracao de arquivos

---
+++ fundamentos/bloco-2-git-github-e-Internet/dia-2-github-entendendo-os-comandos/minhasAnotações.js
@@ -0,0 +1,24 @@
+/*                                Git & GitHub - Entendendo os comandos
+  
+git clone = voce clona o repositório já existente.
+git clone + ulr "do repositorio alvo" = Crie uma pasta para receber estes arquivos  e navegue dentro dela depois use o comando.
+git log --diff-filter=D --summary = ex: de uma variação do "git log"mostra todo o histórico + " --diff-filter=D" filta a pesquisa usando a referência "D" arquivos deletados
+ e o " --summary" como deve ser exibido.
+git rm "arquivo" = remove o arquivo direcionado
+Git ignore =  primeiro passo o arquivo deve ser criado dentro da pasta do git com nome .gitignore depois acesse o arquivo usando o "cat" após o acesso voce definirá quais arquivo deverá ser ignorados
+pelo git sendo "*.txt" excluir por extenção "nome.txt" excluir por tipo especifico ou diretório "exercicios/" O barra representa a extensão das sub-pasta que tambem será ignoradas.
+git ignore = Obs: sempre fazer commit -m para identificar o que está sendo ignorado caso aconteça.
+git fetch = O comando faz a checagem se houve alteração no arquivo lá na main/master , isso garante
+que voce está trabalhando na vesão mais atualizada do arquivo . Muito útil para uso em que o arquivo e compartilhado com mais pessoas envolvidas.
+
+
+
+
+
+
+
+
+
+
+
+*/                               
